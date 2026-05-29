f5442599730697ee81413bfc5edeb2413e0fb7f5
TemurMalik <tmaq77@gmail.com>
2026-05-29 22:17:44 +0500

    SkladProBackend

--- config/db.js
+++ config/db.js
@@ -7,6 +7,12 @@ const sequelize = new Sequelize(
   {
     dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
