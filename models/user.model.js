9d9484c98bf7c65b47d3bb873ae163534ccdcee7
TemurMalik <tmaq77@gmail.com>
2026-06-03 14:17:07 +0500

    SladProBackend

--- models/user.model.js
+++ models/user.model.js
@@ -1,17 +1,21 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
-const bcrypt = require('bcrypt');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+const bcrypt = require("bcrypt");
 
-const User = sequelize.define('user', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  username: { type: DataTypes.STRING },
-  firstname: { type: DataTypes.STRING },
-  lastname: { type: DataTypes.STRING },
-  gender: { type: DataTypes.ENUM("erkak", "ayol") },
-  age: { type: DataTypes.INTEGER },
-  password: { type: DataTypes.STRING },
-  role: { type: DataTypes.ENUM("admin", "user"), defaultValue: "user" }
-}, { timestamps: true });
+const User = sequelize.define(
+  "user",
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    username: { type: DataTypes.STRING },
+    firstname: { type: DataTypes.STRING },
+    lastname: { type: DataTypes.STRING },
+    gender: { type: DataTypes.ENUM("erkak", "ayol") },
+    age: { type: DataTypes.INTEGER },
+    password: { type: DataTypes.STRING },
+    role: { type: DataTypes.ENUM("admin", "user"), defaultValue: "user" },
+  },
+  { timestamps: true },
+);
 
 User.prototype.validPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
